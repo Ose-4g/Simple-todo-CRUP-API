f0150d5e06527e20686697868888252bd4819c27
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2022-03-17 12:11:38 +0100

    feat: add cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
 const TodoRepository = require('./TodoRepository')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 const repository = new TodoRepository()
 
